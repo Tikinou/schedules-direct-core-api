0ea6189122f0c56c6350f6f73f524d7225cfe0af
tafypz <sebastien.astie@gmail.com>
2013-12-28 08:32:39 -0500

    Initial Commit

--- src/main/java/com/tikinou/schedulesdirect/core/domain/VersionNotSupportedException.java
+++ src/main/java/com/tikinou/schedulesdirect/core/domain/VersionNotSupportedException.java
@@ -52,7 +52,6 @@ public class VersionNotSupportedException extends Exception {
      *                {@link #getCause()} method).  (A <tt>null</tt> value is
      *                permitted, and indicates that the cause is nonexistent or
      *                unknown.)
-     * @since 1.4
      */
     public VersionNotSupportedException(String message, Throwable cause) {
         super(message, cause);
@@ -63,14 +62,12 @@ public class VersionNotSupportedException extends Exception {
      * message of <tt>(cause==null ? null : cause.toString())</tt> (which
      * typically contains the class and detail message of <tt>cause</tt>).
      * This constructor is useful for exceptions that are little more than
-     * wrappers for other throwables (for example, {@link
-     * java.security.PrivilegedActionException}).
+     * wrappers for other throwables.
      *
      * @param cause the cause (which is saved for later retrieval by the
      *              {@link #getCause()} method).  (A <tt>null</tt> value is
      *              permitted, and indicates that the cause is nonexistent or
      *              unknown.)
-     * @since 1.4
      */
     public VersionNotSupportedException(Throwable cause) {
         super(cause);
